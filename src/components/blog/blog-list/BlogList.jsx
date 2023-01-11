9dc5ed74a22fa73b9872a19f0bf6ccf99bf4dd4b
SandroIerna <iernasandro@gmail.com>
2023-01-11 16:30:13 +0100

    a

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,21 +1,43 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import posts from "../../../data/posts.json";
+import { useEffect, useState } from "react";
+
 import BlogItem from "../blog-item/BlogItem";
 
 const BlogList = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getBlogPosts = async () => {
+    try {
+      let res = await fetch(
+        "https://u2-d3-backend-production.up.railway.app/articles"
+      );
+      if (res.ok) {
+        let data = await res.json();
+        setPosts(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getBlogPosts();
+  }, []);
   return (
     <Row>
-      {posts.map((post) => (
-        <Col
-          md={4}
-          style={{
-            marginBottom: 50,
-          }}
-        >
-          <BlogItem key={post.title} {...post} />
-        </Col>
-      ))}
+      {posts &&
+        posts.map((post) => (
+          <Col
+            md={4}
+            style={{
+              marginBottom: 50,
+            }}
+            key={post._id}
+          >
+            <BlogItem key={post.title} {...post} />
+          </Col>
+        ))}
     </Row>
   );
 };
